a6a339e4dcb59bbb4211823993c7f3b0422c2b93
leon0113 <tahjib.leon399@gmail.com>
2022-08-21 21:52:46 +0600

    case button solution

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-document.getElementById('case-plus').addEventListener('click', function () {
+// ! common function for both case + - button 
+function updateCaseNumber(isIncreasing) {
     const caseInput = document.getElementById('case-num');
-    const caseNumber = caseInput.value;
-    caseInput.value = parseInt(caseNumber) + 1;
-    console.log(caseNumber);
+    let caseNumber = caseInput.value;
+    if (isIncreasing == true) {
+        caseNumber = parseInt(caseNumber) + 1;
+    }
+    else if (caseNumber > 0) {
+        caseNumber = parseInt(caseNumber) - 1;
+    }
+    caseInput.value = caseNumber;
+    // ! updated case total
+    const caseTotal = document.getElementById('case-total');
+    caseTotal.innerText = caseNumber * 59;
+}
+
+// ! for case plus button
+document.getElementById('case-plus').addEventListener('click', function () {
+    updateCaseNumber(true);
+})
+
+//! for case minus button
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateCaseNumber(false);
 })
